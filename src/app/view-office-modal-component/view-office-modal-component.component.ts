9c5acdab2ce7181691350752435b0e21773d2de5
arjuntechpool <arjun@techpool.co.in>
2025-03-17 12:20:23 +0530

    Data Adding

--- src/app/view-office-modal-component/view-office-modal-component.component.ts
+++ src/app/view-office-modal-component/view-office-modal-component.component.ts
@@ -1,15 +1,15 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 export interface Office {
-  code: string;
-  head: string;
-  shortDesc: string;
+  Office_code: string;
+  Office_id: string;
+  Office_Name: string;
 }
 
 @Component({
@@ -22,20 +22,21 @@ export interface Office {
     MatPaginatorModule,
     MatDialogModule,
     MatFormFieldModule,
+    MatInputModule,
   ],
   styleUrls: ['./view-office-modal-component.component.css']
 })
 export class ViewOfficeModalComponentComponent implements OnInit {
-  displayedColumns: string[] = ['code', 'head', 'shortDesc', 'action'];
+  displayedColumns: string[] = ['Office_id', 'Office_code', 'Office_Name', 'action'];
   dataSource = new MatTableDataSource<Office>();
   searchQuery: string = '';
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   offices: Office[] = [
-    { code: '110010000', head: 'Property Tax (for General Purpose)', shortDesc: 'Property Tax' },
-    { code: '110020000', head: 'Water Tax', shortDesc: 'Water Tax' },
-    { code: '110030000', head: 'Sewerage Tax', shortDesc: 'Sewerage Tax' },
+    { Office_id: '658', Office_code: '998', Office_Name: 'Tvm Main' },
+    { Office_id: '659', Office_code: '999', Office_Name: 'Kollam Main' },
+    { Office_id: '700', Office_code: '1000', Office_Name: 'Thrissur H.O' },
     // Add more offices as needed
   ];
 
@@ -46,6 +47,10 @@ export class ViewOfficeModalComponentComponent implements OnInit {
 
   ngOnInit(): void {
     this.dataSource.data = this.offices;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
   }
 
