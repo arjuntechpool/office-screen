dcdc0e1868f11d255bb8e90eae3bb018efad26fa
arjuntechpool <arjun@techpool.co.in>
2025-03-17 12:30:02 +0530

    Need Style

--- src/app/office-cadre/office-cadre.component.ts
+++ src/app/office-cadre/office-cadre.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ViewOfficeModalComponentComponent } from '../view-office-modal-component/view-office-modal-component.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface Office {
+  Office_Name: string;
+  Office_code: string;
+}
 
 @Component({
   selector: 'app-office-cadre',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './office-cadre.component.html',
   styleUrls: ['./office-cadre.component.css']
 })
@@ -20,26 +26,34 @@ export class OfficeCadreComponent {
 
   constructor(private dialog: MatDialog) {}
 
-  selectText(event: Event): void {
-    const dialogRef = this.dialog.open(ViewOfficeModalComponentComponent, {
-      width: '800px', // Set a fixed width
-      maxHeight: '80vh', // Set a max height
-    });
+ selectText(event: Event): void {
+  const dialogRef = this.dialog.open(ViewOfficeModalComponentComponent, {
+    width: '800px',
+    maxHeight: '80vh',
+  });
 
-    dialogRef.afterClosed().subscribe((selectedOffice: any) => {
-      if (selectedOffice) {
-        this.model.office = selectedOffice.head;
-      }
-    });
-  }
+  dialogRef.afterClosed().subscribe((selectedOffice: Office | undefined) => {
+    if (selectedOffice) {
+      this.model.office = selectedOffice.Office_Name; // Set selected office name
+      this.populateOfficers(selectedOffice.Office_code); // Populate officers based on office code
+    }
+  });
+}
+
 
   populateOfficers(officeCode: string): void {
-    // Example: Fetch officers based on office code
-    if (officeCode === '110010000') {
-      this.officers = ['Manager 1', 'Manager 2', 'Manager 3'];
-    } else if (officeCode === '110020000') {
-      this.officers = ['Manager A', 'Manager B'];
+    switch (officeCode) {
+      case '998':
+        this.officers = ['Manager 1', 'Manager 2', 'Manager 3'];
+        break;
+      case '999':
+        this.officers = ['Manager A', 'Manager B'];
+        break;
+      case '1000':
+        this.officers = ['Manager X', 'Manager Y', 'Manager Z'];
+        break;
+      default:
+        this.officers = [];
     }
-    // Add more logic as needed
   }
 }
